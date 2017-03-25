a465e67b77b95b2d3f90aa6f185fe756e00d3631
JING ZHU <gin@timezenstudio.com>
2017-03-25 15:08:01 +0800

    Add booking model

---
+++ db/migrate/20170325065633_create_bookings.rb
@@ -0,0 +1,13 @@
+class CreateBookings < ActiveRecord::Migration[5.0]
+  def change
+    create_table :bookings do |t|
+      t.datetime :start_time
+      t.datetime :end_time
+      t.references :user, foreign_key: true
+      t.references :skill, foreign_key: true
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
