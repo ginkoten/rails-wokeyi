01af690e5626ad98d069ce861465276d2f526869
JING ZHU <gin@timezenstudio.com>
2017-03-25 15:01:02 +0800

    Add null constrains to user_profile

--- db/migrate/20170325061202_create_user_profiles.rb
+++ db/migrate/20170325061202_create_user_profiles.rb
@@ -1,11 +1,11 @@
 class CreateUserProfiles < ActiveRecord::Migration[5.0]
   def change
     create_table :user_profiles do |t|
-      t.string :full_name
-      t.datetime :birth_day
-      t.string :gender
-      t.string :phone_number
-      t.references :user, foreign_key: true
+      t.string :full_name,                     null:true
+      t.datetime :birth_day,                   null:true
+      t.string :gender,                        null:true
+      t.string :phone_number,                  null:true
+      t.references :user, foreign_key: true,   null:false
 
       t.timestamps
     end
