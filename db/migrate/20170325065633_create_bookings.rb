801b8d15798095a615b3f0e6e1d8b9247737eddc
JING ZHU <gin@timezenstudio.com>
2017-03-25 15:16:15 +0800

    Add null constrains to bookings

--- db/migrate/20170325065633_create_bookings.rb
+++ db/migrate/20170325065633_create_bookings.rb
@@ -1,11 +1,11 @@
 class CreateBookings < ActiveRecord::Migration[5.0]
   def change
     create_table :bookings do |t|
-      t.datetime :start_time
-      t.datetime :end_time
-      t.references :user, foreign_key: true
-      t.references :skill, foreign_key: true
-      t.string :status
+      t.datetime :start_time,                 null:false
+      t.datetime :end_time,                   null:false
+      t.references :user, foreign_key: true,  null:false
+      t.references :skill, foreign_key: true, null:false
+      t.string :status,                       null:false
 
       t.timestamps
     end
